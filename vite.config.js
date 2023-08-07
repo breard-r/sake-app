e3d97cff2242154ba978ddc66386a0475a0bd5a5
Rodolphe Bréard <rodolphe@what.tf>
2023-08-07 17:23:40 +0200

    Disable legacy API

--- vite.config.js
+++ vite.config.js
@@ -22,5 +22,10 @@ export default defineConfig({
 				assetFileNames: 'assets/sake.min[extname]',
 			}
 		}
+	},
+	define: {
+		__VUE_I18N_FULL_INSTALL__: true,
+		__VUE_I18N_LEGACY_API__: false,
+		__INTLIFY_PROD_DEVTOOLS__: false,
 	}
 })
